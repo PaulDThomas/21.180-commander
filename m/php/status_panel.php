c8e72f7a258030b639b58cc9dd1b9a757a561409
WiiMoritz <fernando16@gmail.com>
2015-12-29 14:20:55 -0200

    changed status panel to hide future phases

--- m/php/status_panel.php
+++ m/php/status_panel.php
@@ -6,6 +6,9 @@
 // $Id: status_panel.php 237 2014-07-10 07:28:53Z paul $
 
 $last_phase='';
+$showall = false; // If you want to make it as option or by espionage level
+$firstdone = false;
+$powerlist = array();
 $query = "
 Select powername, phasedesc
        ,Case
@@ -20,21 +23,43 @@ Where gameno=$gameno and (ordername='ORDSTAT' or substring(ordername,1,3)='MA_')
 ";
 
 $result = $mysqli -> query($query) or die ($mysqli -> error);
-if ($result -> num_rows > 0) { while ($row = $result -> fetch_row()) {
-    $tabpowername = $row[0];
-    $phasedesc = $row[1];
-    $order_code = $row[2];
-    $mia = $row[3];
-
-    if ($last_phase != $phasedesc) {?><thead><tr><th colspan="2"><?php echo $phasedesc; ?></th></tr></thead><?php }
-    $last_phase=$phasedesc;
-    ?><tr>
-        <td><?php
-            if ($mia >= 3) echo "<em>$tabpowername (MIA)</em>";
-            else if($powername == $tabpowername) echo "<strong>$tabpowername</strong>";
-            else echo $tabpowername;
-        ?>
-        </td>
-        <td><?php echo $order_code; ?></td>
-    </tr><?php } $result -> close(); } ?>
+if ($result -> num_rows > 0) {
+	while ($row = $result -> fetch_row()) {
+		$tabpowername = $row[0];	
+		$phasedesc = $row[1];
+		$order_code = $row[2];
+		$mia = $row[3];
+		if (($last_phase != '') && ($last_phase != $phasedesc)) {
+			$firstdone = true;
+		}
+		if ((($last_phase != $phasedesc) && (!$firstdone)) || (($last_phase != $phasedesc) && ($showall))) {
+			echo "<thead><tr><th colspan='2'>$phasedesc</th></tr></thead>";
+			$last_phase=$phasedesc;		
+		}
+		if ($showall || !$firstdone) {
+			echo "<tr><td>";
+			if ($mia >= 3) {
+				echo "<em>$tabpowername (MIA)</em>";
+			} else if($powername == $tabpowername) {
+				echo "<strong>$tabpowername</strong>";
+			} else {
+				echo $tabpowername;
+			}       
+			echo "</td><td>$order_code</td></tr>";    
+		}
+		if (($powername == $tabpowername) && ($order_code != "Passed") && ($firstdone)) {
+			if (!in_array($phasedesc, $powerlist)) {
+				$powerlist[] = $phasedesc;
+			}
+		}
+	}
+	$result -> close();
+	if (!$showall && (count($powerlist) > 0)) {
+		echo "<thead><tr><th colspan='2'>The Next Phases You Selected</th></tr></thead>";
+		foreach ($powerlist AS $desc) {
+			echo "<tr><td colspan='2'>$desc</td></tr>";
+		}
+	}
+}
+?>
 </table>
